348b4729c8bcae3c2fe766b8e22ef70ba00088f0
Veronika Niema <vorokita@gmail.com>
2025-12-11 00:38:50 +0200

    add

---
+++ TextEditor/Components/Editor.razor.cs
@@ -0,0 +1,39 @@
+using Microsoft.AspNetCore.Components;
+using Microsoft.JSInterop;
+
+namespace TextEditor.Components
+{
+    public partial class RichEditor : ComponentBase
+{
+    [Inject]
+    protected IJSRuntime JSRuntime { get; set; }
+
+    protected async Task MakeBold()
+    {
+        await JSRuntime.InvokeVoidAsync("editorActions.execCmd", "bold", "");
+
+    }
+
+    protected async Task MakeItalic()
+        {
+            await JSRuntime.InvokeVoidAsync("editorActions.execCmd", "italic", "");
+        }
+
+        protected async Task MakeUnderline()
+        {
+            await JSRuntime.InvokeVoidAsync("editorActions.execCmd", "underline", "");
+        }
+
+        protected async Task ChangeSize(ChangeEventArgs e)
+        {
+            await JSRuntime.InvokeVoidAsync("editorActions.execCmd", "fontSize", e.Value?.ToString());
+        }
+
+        protected async Task ChangeColor(ChangeEventArgs e)
+        {
+            await JSRuntime.InvokeVoidAsync("editorActions.execCmd", "foreColor", e.Value?.ToString());
+        }
+    }
+
+
+}
